e031100b0704c19a54b63dc5e0a4f07c74140b00
MAzharAR03 <1221100800@student.mmu.edu.my>
2025-09-26 14:54:01 +0800

    Model commit

---
+++ Model.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+public class Model {
+    protected Connection c;
+    protected PreparedStatement s;
+    protected String path;
+
+    Model(String path) {
+        this.path = path;
+    }
+
+    Model() {
+        path = "jdbc:sqlite:DigitalCompDB.db";
+    }
+
+    public void connect(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection(path);
+            System.out.println("Opened database successfully.");
+            c.setAutoCommit(false);
+
+        }catch(Exception e){
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args){
+        Model a = new Model("jdbc:sqlite:DigitalCompDB.db");
+        a.connect();
+    }
+
+}
