c2ad6d9987414bfe63ecfb59367095d61f62d864
MAzharAR03 <1221100800@student.mmu.edu.my>
2025-09-26 18:44:58 +0800

    Transaction Model added

--- Transaction.java
+++ Transaction.java
@@ -1,27 +1,31 @@
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Transaction {
     private String username;
-    private final String type;
-    private final float amount;
+    private final double amount;
     private final String datetime;
     private final String note;
 
-    public Transaction(String username, String type, float amount, String note) {
+    public Transaction(String username, double amount, String note) {
         this.username = username;
-        this.type = type;
         this.amount = amount;
         this.note = note;
-        this.datetime = LocalDateTime.now().toString();
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        this.datetime = LocalDateTime.now().format(myFormatObj);
     }
 
-    public String getType(){
-        return type; }
-    public float getAmount(){
+    public double getAmount(){
         return amount; }
     public String getTimestamp() {
         return datetime; }
     public String getNote() {
         return note; }
 
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
---
+++ TransactionModel.java
@@ -0,0 +1,68 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TransactionModel extends Model{
+    TransactionModel(){
+        super();
+    }
+
+    public void addTransaction(Transaction transaction){
+        try{
+            connect();
+            String query = "INSERT INTO Transactions(Username, Amount, DateTime, Note) VALUES(?,?,?,?)";
+            s = c.prepareStatement(query);
+            s.setString(1,transaction.getUsername());
+            s.setDouble(2,transaction.getAmount());
+            s.setString(3,transaction.getTimestamp());
+            s.setString(4, transaction.getNote());
+            s.executeUpdate();
+            c.commit();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally{
+            try{
+                s.close();
+                c.close();
+            }catch (Exception e){
+                System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+                System.exit(0);
+            }
+        }
+    }
+
+    public float calculateBalance(String username){
+        float balance = 0;
+        ResultSet rs = null;
+        try{
+            connect();
+            String query = "SELECT SUM(Amount) AS Total FROM Transactions WHERE Username =?";
+            s = c.prepareStatement(query);
+            s.setString(1,username);
+            rs = s.executeQuery();
+            if(rs.next()){
+                balance = rs.getFloat("Total");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try{
+                s.close();
+                c.close();
+                try { if (rs != null) rs.close(); } catch (SQLException ignored) {}
+            }catch (Exception e){
+                System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+                System.exit(0);
+            }
+        }
+        return balance;
+    }
+
+    public static void main(String[] args){
+        TransactionModel tm = new TransactionModel();
+        tm.addTransaction(new Transaction("jon",-100.0,"Broke"));
+        tm.addTransaction(new Transaction("azhar",100.0,"Broke"));
+        tm.addTransaction(new Transaction("jon",50.0,"gift"));
+        System.out.println(tm.calculateBalance("jon"));
+    }
+}
